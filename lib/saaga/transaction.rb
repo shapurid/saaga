b217b0086d25e36d12a874ac27c49ff8b6278416
shapurid <shapurid@yandex.ru>
2023-04-11 22:36:28 +0300

    add: execute method with executed_stages_results

--- lib/saaga/transaction.rb
+++ lib/saaga/transaction.rb
@@ -6,10 +6,11 @@ require 'saaga/errors/stage_registration_error'
 module Saaga
   # Temp doc
   class Transaction
-    attr_reader :registered_stages
+    attr_reader :registered_stages, :executed_stages_results
 
     def initialize
       @registered_stages = []
+      @executed_stages_results = {}
     end
 
     def register(stage_name, action, compensation = nil)
@@ -21,6 +22,11 @@ module Saaga
       self
     end
 
+    def execute
+      execute_stages
+      @executed_stages_results
+    end
+
     private
 
     def validate_stage_uniqueness(stage)
@@ -28,6 +34,16 @@ module Saaga
       found_stage = @registered_stages.find { _1.name == stage_name }
 
       raise Errors::StageRegistrationError, "stage with name '#{stage_name.inspect}' already exists" if !!found_stage
+    end
+
+    def execute_stages
+      @registered_stages.each_with_index do |stage, stage_index|
+        @executed_stages_results[stage.name] = stage.execute_action(@executed_stages_results).clone
+      rescue StandardError => error
+        @registered_stages.slice(0, stage_index)
+                          .reverse_each { _1.execute_compensation(@executed_stages_results) }
+        raise error
+      end
     end
   end
 end
