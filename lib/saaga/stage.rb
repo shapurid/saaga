2bf7f454e556ca87a7a729bff262f69907ff916b
shapurid <shapurid@yandex.ru>
2023-04-11 22:35:17 +0300

    refactor: stage with executing logic incapsulation

--- lib/saaga/stage.rb
+++ lib/saaga/stage.rb
@@ -7,6 +7,11 @@ module Saaga
   class Stage
     attr_reader :name, :action, :compensation
 
+    INVALID_NAME_ERROR_MESSAGE = "name is not a 'Symbol'"
+    INVALID_ACTION_ERROR_MESSAGE = "action is not respond to ':call' or has arity more or less 1"
+    INVALID_COMPENSATION_ERROR_MESSAGE = "compensation is not respond to ':call' " \
+                                         "or has arity more or less 1 or not 'nil'"
+
     def initialize(name, action, compensation)
       @name = name
       @action = action
@@ -14,12 +19,20 @@ module Saaga
     end
 
     def validate
-      message = [].then { valid_name? ? _1 : [*_1, "name is not a 'Symbol'"] }
-                  .then { valid_action? ? _1 : [*_1, "action is not respond to ':call'"] }
-                  .then { valid_compensation? ? _1 : [*_1, "compensation is not respond to ':call' or not 'nil'"] }
+      message = [].then { valid_name? ? _1 : [*_1, INVALID_NAME_ERROR_MESSAGE] }
+                  .then { valid_action? ? _1 : [*_1, INVALID_ACTION_ERROR_MESSAGE] }
+                  .then { valid_compensation? ? _1 : [*_1, INVALID_COMPENSATION_ERROR_MESSAGE] }
                   .join(', ')
 
       raise Errors::StageValidationError, message unless message.empty?
+    end
+
+    def execute_action(actions_results)
+      @action.call(actions_results)
+    end
+
+    def execute_compensation(actions_results)
+      @compensation&.call(actions_results)
     end
 
     private
@@ -30,13 +43,23 @@ module Saaga
 
     # :reek:ManualDispatch
     def valid_action?
-      @action.respond_to?(:call)
+      @action.respond_to?(:call) && valid_arity?(@action)
     end
 
     # :reek:ManualDispatch
     # :reek:NilCheck
     def valid_compensation?
-      @compensation.nil? || compensation.respond_to?(:call)
+      @compensation.nil? || (compensation.respond_to?(:call) && valid_arity?(@compensation))
+    end
+
+    # :reek:UtilityFunction
+    def valid_arity?(object)
+      arity = if object.is_a?(Proc)
+                object.arity
+              else
+                object.method(:call).arity
+              end
+      arity == 1
     end
   end
 end
